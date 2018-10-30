64b5018677a4130ee64b4d3892953f585e26f5ec
William Trindade <williamtrindade777@gmail.com>
2018-10-30 14:58:11 -0300

    Update select.sql

--- banco de dados/chave estrangeira/01/select.sql
+++ banco de dados/chave estrangeira/01/select.sql
@@ -1,1 +1,5 @@
-SELECT codigoProduto, descricao FROM produto, venda WHERE venda.codigoCli = 1 AND venda.codigoProd = produto.codigoProduto
+SELECT codigoProduto, descricao FROM produto, venda WHERE venda.codigoCli = 1 AND venda.codigoProd = produto.codigoProduto;
+
+SELECT codigoProduto, descricao FROM produto WHERE codigoProduto NOT IN ( 
+  SELECT codigoProduto FROM produto, venda WHERE venda.codigoCli = 1 AND venda.codigoProd = produto.codigoProduto 
+);
